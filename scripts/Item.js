753385bee1b661006f1e0bb28e4676b0740566fb
Jess Doyle <jessica.e.doyle@gmail.com>
2019-07-17 11:51:49 -0400

    add validateName and create methods to Item

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,8 +1,17 @@
 'use strict';
 
 const Item = (function () {
+  const validateName = function(name) {
+    if (!name) throw new Error('Name does not exist');
+  };
+  const create = function(name) {
+    return { id: cuid(), name: name, checked: false };
+  };
 
-  return {};
+  return {
+    validateName,
+    create,
+  };
 
 }());
 

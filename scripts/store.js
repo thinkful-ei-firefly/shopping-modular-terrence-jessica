3eb45c4a0579c30f8f912722bafd105bc8e8d660
Jess Doyle <jessica.e.doyle@gmail.com>
2019-07-17 11:15:21 -0400

    move store to IIFE

--- scripts/store.js
+++ scripts/store.js
@@ -1,4 +1,23 @@
 'use strict';
+
+const store = (function () {
+  let items = [
+    { id: cuid(), name: 'apples', checked: false },
+    { id: cuid(), name: 'oranges', checked: false },
+    { id: cuid(), name: 'milk', checked: true },
+    { id: cuid(), name: 'bread', checked: false }
+  ];
+  let hideCheckedItems = false;
+  let searchTerm = '';
+
+  return {
+    items,
+    hideCheckedItems,
+    searchTerm
+  };
+
+}() );
+
 /* global shoppingList, cuid */
 
 // eslint-disable-next-line no-unused-vars
@@ -13,11 +32,12 @@ const store = {
   hideCheckedItems: false,
   searchTerm: ''
 };
-*/
-
+  
 function main() {
   shoppingList.bindEventListeners();
   shoppingList.render();
 }
-
+  
 $(main);
+*/
+  
